42935829d23b7f5100610d53cdbebe4439a2aecb
Nguyen Thanh Nhan <ngthnhan93@gmail.com>
2014-08-02 15:57:56 +0800

    Changing cutoff time to earlier time

--- settings.py
+++ settings.py
@@ -8,6 +8,6 @@ API_KEY='AIzaSyAiSOOzgTo9ItnSRQ1TRJI1FHOGJTZg7es'
 # Generate Tour Constants
 SUITABILITY_GAIN = 50
 ESP = 0.5
-TOUR_CUTOFF_TIME = datetime.time(22,0)
+TOUR_CUTOFF_TIME = datetime.time(20,0)
 TOUR_START_TIME = datetime.time(8,0)
 
